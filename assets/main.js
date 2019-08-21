dfb70d8b3690528ebadfdebfacfec3d9bf5bf135
buddison <buddison@gmail.com>
2019-08-21 12:28:46 +0700

    Change coords entry to in-game coords, fix calculation bug

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,39 @@
-var origin_x, origin_y, destination_x, destination_y, march_bonus, troop_rate;
+var origin, destination, march_bonus, troop_rate;
 
 $(function(){
     function calc(){
-        origin_x = parseFloat($("#origin_x").val());
-        origin_y = parseFloat($("#origin_y").val());
-        destination_x = parseFloat($("#destination_x").val());
-        destination_y = parseFloat($("#destination_y").val());
+        // origin_x = parseFloat($("#origin_x").val());
+        // origin_y = parseFloat($("#origin_y").val());
+        // destination_x = parseFloat($("#destination_x").val());
+        // destination_y = parseFloat($("#destination_y").val());
+        const orig_val  = $("#origin").val();
+        const origin    =   orig_val
+                            .match(/[A-Z]:\s?\d+/g)
+                            .reduce((coords, str) => {
+                                const [key, value] = str.split(':')
+                                return {
+                                    ...coords,
+                                    [key]: value.trim(),
+                                }
+                            }, {});
+        const destination_val  = $("#destination").val();
+        const destination    =   destination_val
+                            .match(/[A-Z]:\s?\d+/g)
+                            .reduce((coords, str) => {
+                                const [key, value] = str.split(':')
+                                return {
+                                    ...coords,
+                                    [key]: value.trim(),
+                                }
+                            }, {});
         march_bonus = parseFloat($("#march_bonus").val());
         troop_rate = parseFloat($("#troop_rate").val()) || 2;
+        console.log(origin);
+        console.log(destination);
 
-        let circle1 = Math.sqrt(Math.pow(origin_x-destination_x,2) + Math.pow(origin_y*2-destination_y*2,2));
-        let circle2 = Math.sqrt(Math.pow(origin_x-destination_x,2) + Math.pow(origin_y*2-(origin_x&1)-destination_y*2+(origin_y&1),2));
-        let oval = Math.sqrt(Math.pow(origin_x-destination_x,2) + Math.pow(origin_y-destination_y,2));
+        let circle1 = Math.sqrt(Math.pow(origin.X-destination.X,2) + Math.pow(origin.Y*2-destination.Y*2,2));
+        let circle2 = Math.sqrt(Math.pow(origin.X-destination.X,2) + Math.pow(origin.Y*2-(origin.X&1)-destination.Y*2+(origin.Y&1),2));
+        let oval = Math.sqrt(Math.pow(origin.X-destination.X,2) + Math.pow(origin.Y-destination.Y,2));
         let fast_march =  3 * oval * 100 / (100 + march_bonus) * 2 / troop_rate;
         let slow_march = 15 * oval * 100 / (100 + march_bonus) * 2 / troop_rate;
         $("#result").text(circle1.toFixed(2) + ' (' + circle2.toFixed(2) + ')');
